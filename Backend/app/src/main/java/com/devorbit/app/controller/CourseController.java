79bb632987d4aa45a22a970f2c62f4625a43c450
FallasDev <sebas10stiven212@gmail.com>
2025-05-02 20:26:21 -0600

    merge ramas

--- Backend/app/src/main/java/com/devorbit/app/controller/CourseController.java
+++ Backend/app/src/main/java/com/devorbit/app/controller/CourseController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 // Swagger
 
 @RestController
-@RequestMapping("/curses")
+@RequestMapping("/api/courses")
 public class CourseController {
 
     @Autowired
